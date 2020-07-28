181211e12181a9514d32ea164334a38ee41fe8d3
Maksim Verkhoturov <sd32@protonmail.com>
2020-07-28 11:56:31 -0600

    artist query logic

--- song.js
+++ song.js
@@ -1,2 +1,23 @@
 const inquirer = require("inqurier");
 const mysql = require("mysql");
+
+const connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "",
+    database: "top_songsDB"
+});
+
+const artistQuery = artistName => {
+    connection.query(`SELECT * FROM top5000 WHERE artist = ${artistName}`, (err, data) => {
+        if (err) throw err;
+        console.log(data);
+    });
+}
+
+connection.connect(err => {
+    if (err) throw err;
+    console.log(`Connected as id ${connection.threadId}`);
+    artistQuery("Beatles");
+});
